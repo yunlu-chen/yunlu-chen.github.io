2f89c2e053ff79dc7581baa858b4b13f6cacd2cf
yunlu-chen <rex_cyl@hotmail.com>
2022-02-09 04:03:47 +0100

    Add files via upload

---
+++ Yunlu Chen_files/script.js
@@ -0,0 +1,32 @@
+function toggleblock(blockId)
+{
+    var block = document.getElementById(blockId);
+    if (block.style.display == 'none') {
+	block.style.display = 'block' ;
+    } else {
+	block.style.display = 'none' ;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
